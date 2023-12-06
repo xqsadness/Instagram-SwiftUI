9b936e93956299104ee264a4ae190d163ee71d96
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2023-12-06 16:33:57 +0700

    init

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by darktech4 on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
